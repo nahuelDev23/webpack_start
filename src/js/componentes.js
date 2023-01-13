371dd172f14090e28415deaace1539e30725334b
NahuelDev23 <nahuel.dev.23@gmail.com>
2023-01-13 10:56:16 -0300

    first commit

---
+++ src/js/componentes.js
@@ -0,0 +1,14 @@
+import '../css/componentes.css'
+//import webpacklogo  from '../assets/img/webpack-logo.png';
+
+export const saludar = () => {
+	const h1 =document.createElement('h1');
+	h1.innerText = "Nawe";
+
+
+	//const image = document.createElement('img')
+	image.src =  webpacklogo
+	document.body.append(h1)
+
+	//document.body.append(image)
+}
